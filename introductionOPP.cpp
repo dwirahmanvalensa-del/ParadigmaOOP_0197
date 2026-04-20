e174a17e96a16461a567aa366688e186889e0ed8
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-04-20 14:30:33 +0800

    membuat fungsi printData

--- introductionOPP.cpp
+++ introductionOPP.cpp
@@ -7,4 +7,11 @@ public :
     string nama;
     float nilai;
 
-    
+    void printData(){
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+
+    }
+}; //batas class
+
